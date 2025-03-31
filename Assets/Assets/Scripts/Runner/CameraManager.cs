7bba17a7f9d830f9e2faf5e4a39ce3ed78531c3a
Kylan <1033544@student.sae.edu.au>
2025-03-31 13:32:25 +1000

    fixed runner camera and added some functionality
    The runner camera does not displace along the y axis
    the runner now stops moving if blocked by an obstacle
    the runner game is now actually flappy bird somehow and honestly its pretty fun

--- Assets/Assets/Scripts/Runner/CameraManager.cs
+++ Assets/Assets/Scripts/Runner/CameraManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] GameObject runner;
+    private Vector3 newLocation;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,7 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        newLocation = runner.transform.position;
+        this.transform.position = new Vector3(newLocation.x, 0f, 0f);
     }
 }
--- Assets/Assets/Scripts/Runner/RunnerMovement.cs
+++ Assets/Assets/Scripts/Runner/RunnerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RunnerMovement : MonoBehaviour
@@ -8,8 +9,9 @@ public class RunnerMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 20f;
     [SerializeField] private float runSpeed = 2f;
     [SerializeField] private float speedMult = 1f;
+    [SerializeField] private float rayDist = 1f;
     public float horizontalDist = 0f;
-
+    private Vector3 rayDisplace = new Vector3 (0.51f, 0f, 0f);
     private void Jump()
     {
         rb2d.AddForce(new Vector2(0, jumpForce));
@@ -25,9 +27,6 @@ public class RunnerMovement : MonoBehaviour
         }
     }
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -37,16 +36,28 @@ public class RunnerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1*runSpeed, 0) * Time.deltaTime;
-        runSpeed += Time.deltaTime * (speedMult * 0.01f);
+        //check if the runner is blocked by an obstacle in front
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position + rayDisplace, Vector3.right, rayDist);
+        if (raycastHit2D && raycastHit2D.collider.gameObject.tag == "Platform")
+        {
+            print(raycastHit2D.collider.name);
+        }
+        else
+        {
+            transform.position += new Vector3(1 * runSpeed, 0) * Time.deltaTime;
+            runSpeed += Time.deltaTime * (speedMult * 0.01f);
+        }
+
+        Debug.DrawRay(transform.position + rayDisplace, Vector3.right*rayDist, Color.red);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
+
         if (horizontalDist < transform.position.x)
         {
             horizontalDist = transform.position.x;
         }
-
     }
 }
